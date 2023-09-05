6bd74dc5e3193c96f4c6f3e96bf484cbff5cf2a3
Morgan Kruger <nagromkruger@student.cvtc.edu>
2023-09-04 19:45:33 -0500

    Added routing to messages component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { MessageComponent } from './message/message.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MessageDetailComponent } from './message-detail/message-detail.component';
-=======
 import { HeaderComponent } from './header/header.component';
->>>>>>> 50b773971f83e318e97c62e3c8df6726a30c1470
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     MessageComponent,
     MessageDetailComponent,
-=======
     HeaderComponent
->>>>>>> 50b773971f83e318e97c62e3c8df6726a30c1470
   ],
   imports: [
     BrowserModule,
